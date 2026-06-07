bb9d0a5c2f88ade9957f920ee9b6b07466b25608
Apolo Medina Cshunderlick <apololick2@gmail.com>
2026-06-07 15:29:03 -0300

    Postando api

--- frontend/src/home.tsx
+++ frontend/src/home.tsx
@@ -112,7 +112,7 @@ export function Home() {
     setTouched({ name: true, email: true, password: true });
     if (!errors.name && !errors.email && !errors.password) {
       try {
-        const resp = await fetch("http://localhost:3000/user/login", {
+        const resp = await fetch("http://https://api-authhub.onrender.com/user/login", {
           method: "POST",
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify({ name, email, password }),
