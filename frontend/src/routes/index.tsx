4fb2c4ca058361e69fce968b1187f0bb03cfacc3
Apolo Medina Cshunderlick <apololick2@gmail.com>
2026-06-07 19:33:53 -0300

    Atualizando API 2

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -2,13 +2,16 @@ import { Routes, Route } from 'react-router-dom';
 import { Home } from '../home';
 import { Profile } from '../profile';
 import { ProfileWhenCreated } from '../profile/data';
+import { Layout } from '../layout';
 
 export function RouteApp() {
     return (
         <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/profiledata" element={<Profile />} />
-            <Route path="/profilewhencreated" element={<ProfileWhenCreated />} />
+            <Route path="/" element={<Layout />} >
+                <Route index element={<Home />} />
+                <Route path="/profiledata" element={<Profile />} />
+                <Route path="/profilewhencreated" element={<ProfileWhenCreated />} />
+            </Route>
         </Routes>
     )
 }
